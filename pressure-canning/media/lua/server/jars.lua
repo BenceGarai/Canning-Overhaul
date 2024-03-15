5262975267378f210ca1919649db6e4197f0b209
Bence Garai <55026217+BenceGarai@users.noreply.github.com>
2024-03-15 00:26:34 -0700

    Add rest of preserved items

--- pressure-canning/media/lua/server/jars.lua
+++ pressure-canning/media/lua/server/jars.lua
@@ -1,11 +1,9 @@
-function JarredFood_OnCooked(jarredFood)
-    jarredFood:setOffAgeMax(400);
-    jarredFood:setOffAge(360);
-    jarredFood:setAge(0);
-end
-
 function Recipe.OnCreate.OpenJarredFood(items,result,player)
     player:getInventory():AddItem("Base.EmptyJar");
     player:getInventory():AddItem("Base.JarLid");
     --player:Say("Openning Canned Food")
+end
+
+function Recipe.OnCreate.PreserveJarOfFood(items,result,player)
+    result:setAge(0.1);
 end
