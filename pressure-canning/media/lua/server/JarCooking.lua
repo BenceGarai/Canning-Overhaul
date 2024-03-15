6cbad9eb2b9754e676a95ff55f501c67e39c5712
Bence Garai <55026217+BenceGarai@users.noreply.github.com>
2024-03-14 19:06:18 -0700

    Remove duplicate lua files

--- pressure-canning/media/lua/server/JarCooking.lua
+++
@@ -1,6 +0,0 @@
-function OnCooked.JarredFood(jarredFood)
-    local aged = jarredFood:getAge() / jarredFood:getOffAgeMax()
-    jarredFood:setOffAgeMax(400)
-    jarredFood:setOffAge(360)
-    jarredFood:setAge(jarredFood:getOffAgeMax() * aged)
-end
--- pressure-canning/media/lua/server/JarOpening.lua
+++
@@ -1,5 +0,0 @@
-function Recipe.OnCreate.OpenJarredFood(items,result,player)
-    player:getInventory():AddItem("Base.EmptyJar");
-    player:getInventory():AddItem("Base.JarLid");
-    player:Say("Openning Canned Food")
-end
