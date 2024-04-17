76052d5db680d7cead741726b265e4ba1c48a0f7
Bence Garai <55026217+BenceGarai@users.noreply.github.com>
2024-04-17 09:39:14 -0700

    Add new spawn locations for boxes of jars

---
+++ canning-overhaul/media/lua/server/Items/CanningOverhaulDistributions.lua
@@ -0,0 +1,19 @@
+require 'Items/ProceduralDistributions'
+
+-- Box of Jars in houses
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, "Base.BoxOfJars");
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, 3);
+table.insert(ProceduralDistributions["list"]["KitchenDishes"].items, "Base.BoxOfJars");
+table.insert(ProceduralDistributions["list"]["KitchenDishes"].items, 3);
+table.insert(ProceduralDistributions["list"]["KitchenPots"].items, "Base.BoxOfJars");
+table.insert(ProceduralDistributions["list"]["KitchenPots"].items, 3);
+
+-- Box of jars in stores
+table.insert(ProceduralDistributions["list"]["WesternKitchenSauce"].items, "Base.BoxOfJars");
+table.insert(ProceduralDistributions["list"]["WesternKitchenSauce"].items, 3);
+table.insert(ProceduralDistributions["list"]["StoreKitchenSauce"].items, "Base.BoxOfJars");
+table.insert(ProceduralDistributions["list"]["StoreKitchenSauce"].items, 3);
+table.insert(ProceduralDistributions["list"]["MexicanKitchenSauce"].items, "Base.BoxOfJars");
+table.insert(ProceduralDistributions["list"]["MexicanKitchenSauce"].items, 3);
+table.insert(ProceduralDistributions["list"]["BurgerKitchenSauce"].items, "Base.BoxOfJars");
+table.insert(ProceduralDistributions["list"]["BurgerKitchenSauce"].items, 3);
